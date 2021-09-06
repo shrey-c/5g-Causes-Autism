a61907130cc88e850aec14b5ee2aea5a885f24b1
shristi202 <shristihoro202@gmail.com>
2021-09-06 21:03:16 +0530

    error request handling

--- Service/Service API.zip_expanded/Service-API/src/main/java/com/telstra/controller/HomeController.java
+++ Service/Service API.zip_expanded/Service-API/src/main/java/com/telstra/controller/HomeController.java
@@ -1,14 +1,29 @@
 package com.telstra.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController{
+public class HomeController implements ErrorController{
+	
+	private static final String PATH="/error";
+	
 	@RequestMapping(value="/",method= RequestMethod.GET)
     public String helloWorld(){
         return "Hello to Register Service";
     }
+	
+	@RequestMapping(value=PATH, method=RequestMethod.GET)
+    public String DefaultErrorMessage() {
+    	return "Requested Resource is not Found!";
+    }
+
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return PATH;
+	}
 
 }
--- songs/src/main/java/com/songweather/controller/HelloController.java
+++ songs/src/main/java/com/songweather/controller/HelloController.java
@@ -1,17 +1,29 @@
 package com.songweather.controller;
 
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("son")
-public class HelloController {
+//@RequestMapping("son")
+public class HelloController implements ErrorController{
 	
+	private static final String PATH="/error";
 	@RequestMapping(value="/",method= RequestMethod.GET)
     public String helloWorld(){
         return "Hello spring boot";
     }
+
+	@RequestMapping(value=PATH, method=RequestMethod.GET)
+    public String DefaultErrorMessage() {
+    	return "Requested Resource is not Found!";
+    }
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return PATH;
+	}
 
 }
